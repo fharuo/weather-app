fb990fd3dd9de934d73ea1eef660c995afc4829e
Fabrício Takaki <fabriciohtakaki@gmail.com>
2024-09-05 13:28:19 -0300

    feat: initial components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations"
+import { HttpClientModule } from "@angular/common/http"
+import { FormsModule } from '@angular/forms';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { WheaterHomeComponent } from './modules/weather/page/wheater-home/wheater-home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WheaterHomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    FormsModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
